6bf1ca1442ffb1b1dca6bfe837a68be0071f6bfa
emrrar <avatareigan@gmail.com>
2020-04-16 16:03:17 +0300

    Added a login route in the CalculatorApp.

--- MyCoolWebServer/CalculatorApplication/CalculatorApp.cs
+++ MyCoolWebServer/CalculatorApplication/CalculatorApp.cs
@@ -15,6 +15,12 @@
             appRouteConfig
                 .AddRoute("/", new PostHandler(
                     req => new CalculatorController().Index(req.FormData["operandOne"], req.FormData["operator"], req.FormData["operandTwo"])));
+
+            appRouteConfig
+                .AddRoute("/login", new GetHandler(req => new LoginController().Index()));
+
+            appRouteConfig
+                .AddRoute("/login", new PostHandler(req => new LoginController().Index(req.FormData["username"], req.FormData["passwd"])));
         }
     }
 }
---
+++ MyCoolWebServer/CalculatorApplication/Controllers/LoginController.cs
@@ -0,0 +1,23 @@
+﻿namespace MyCoolWebServer.CalculatorApplication.Controllers
+{
+    using Infrastructure;
+    using Server.Http.Contracts;
+    using System.Collections.Generic;
+
+    public class LoginController : Controller
+    {
+        public IHttpResponse Index() => this.FileViewResponse("\\login", new Dictionary<string, string>()
+        {
+            ["display"] = "none"
+        });
+
+        public IHttpResponse Index(string username, string password)
+        {
+            return this.FileViewResponse("\\login", new Dictionary<string, string>
+            {
+                ["content"] = $"Hi {username}, your password is {password}",
+                ["display"] = "block"
+            });
+        }
+    }
+}
